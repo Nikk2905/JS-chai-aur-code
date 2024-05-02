405d5b67f6be62fb2043bd4547d05aa42bc91ec1
Nikhil Kumar <146857019+Nikk2905@users.noreply.github.com>
2024-05-02 20:03:11 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all Js code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+//code readability should be high
+console.log(3+3);  //correct way
+
+console.log(3
+    +
+3)     //wrong way
+
+console.log("nik");
+
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint => very very big number
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined => undefined
+// symbol => unique
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);  // object
